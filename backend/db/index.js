cd9bc9df1f854d1bc8200796cb2696cc875f70a9
Christoffer <christoffer.friman@grebban.com>
2024-10-26 12:45:35 +0200

    fix: minor fix

--- backend/db/index.js
+++ backend/db/index.js
@@ -15,7 +15,6 @@ module.exports = {
       const recipeQuery = `
         INSERT INTO recipieFavorite (title, image_url, instructions, incoming_id, favorite)
         VALUES ($1, $2, $3, $4, $5)
-        RETURNING *
       `;
 
       const recipeValues = [title, image, instructions, id, favorite];
